5e0fc6c00d8f357f9b1ed585de4e661bcdf7bfd1
Antony K. S. <github@smankusors.com>
2026-09-05 21:27:36 +0700

    chore: add ESLint configuration
    also consolidate imports from expo-router found while linting
    
    Signed-off-by: Antony K. S. <github@smankusors.com>

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { Stack } from 'expo-router';
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
